38bfada5d92323ba3f04e911e46e2367c64d7596
alantothe <rubenamalpartida@gmail.com>
2023-09-13 23:28:45 -0400

    add last message to conversation on message post

--- controllers/messages.js
+++ controllers/messages.js
@@ -2,11 +2,30 @@ import Messages from "../models/Messages.js";
 import Conversation from "../models/Conversation.js";
 
 export const createMessage = async (req, res) => {
-  let { conversationId, senderId, message, img } = req.body;
+  let { senderId, recipientId, message, img } = req.body;
 
   try {
+    // check if there's an existing conversation between the two users
+    let conversation = await Conversation.findOne({
+      members: { $all: [senderId, recipientId], $size: 2 },
+    });
+
+    //  if no conversation exists, create one
+    if (!conversation) {
+      conversation = new Conversation({
+        members: [senderId, recipientId],
+        lastMessage: message, // set message
+      });
+      await conversation.save();
+    } else {
+      // update the last message for the existing conversation
+      conversation.lastMessage = message;
+      await conversation.save();
+    }
+
+    // create the new message and link it to the conversation
     const newMessage = new Messages({
-      conversationId,
+      conversationId: conversation._id,
       senderId,
       message,
       img,
@@ -14,16 +33,42 @@ export const createMessage = async (req, res) => {
 
     const messageSaved = await newMessage.save();
 
-    // Emit the event //
-    req.io.emit("new message", JSON.stringify(messageSaved));
+    // req.io.emit("new message", JSON.stringify(messageSaved));
 
-    // If all operations are successful
     res.status(201).json({
       message: "Message was sent successfully!",
-      message: messageSaved,
+      messageData: messageSaved,
     });
   } catch (error) {
-    // If any operation fails, send the error
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getMessagesBetweenTwo = async (req, res) => {
+  const { member1, member2 } = req.body;
+
+  try {
+    // find the conversation between the two users
+    const conversation = await Conversation.findOne({
+      members: { $all: [member1, member2], $size: 2 },
+    });
+
+    if (!conversation) {
+      return res.status(404).json({
+        message: "No conversation found between the specified users.",
+      });
+    }
+
+    //  all messages linked to the found conversation ids
+    const messages = await Messages.find({
+      conversationId: conversation._id,
+    }).sort({ createdAt: 1 }); // sort by creation time, oldest first
+
+    res.status(200).json({
+      message: "Messages fetched successfully.",
+      messages: messages,
+    });
+  } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
